1df532e730ba26487a65f81af0045d2d84909a49
Romain niam0r <niamor@mm.st>
2018-04-06 15:34:22 +0100

    implemented programmatic nav

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const API_URL = 'http://localhost:3090';
 
@@ -6,14 +7,19 @@ export function signinUser({ email, password }) {
   // redux-thunk give us access to dispatch
   return function(dispatch) {
     //  submit email and password to the server
-    axios.post(`${API_URL}/signin`, { email, password });
+    axios.post(`${API_URL}/signin`, { email, password })
+      .then(response => {
+        // if req is good:
+        // - update state to indicate user is auth'd
+        // - save the JWT token
 
-    // if req is good:
-    // - update state to indicate user is auth'd
-    // - save the JWT token
-    // - redirect to the route '/feature'
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // if req is bad:
+        // - show error to the user
 
-    // if req is bad:
-    // - show error to the user
+      });
   }
 }
