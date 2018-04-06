ffc11a94e7815eb991ab6ee4433643216bda050e
Romain niam0r <niamor@mm.st>
2018-04-06 17:18:37 +0100

    implement header logic

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link to="/signout" className="nav-link">Sign Out</Link>
+        </li>
+      );
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link to="/signin" className="nav-link">Sign In</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link to="/signup" className="nav-link">Sign Up</Link>
+        </li>
+      ];
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-            <li className="nav-item">
-              Sign in
-            </li>
+        <nav className="navbar navbar-light navbar-expand-sm">
+          <Link to="/" className="navbar-brand">Redux Auth</Link>
+          <ul className="navbar-nav">
+            {this.renderLinks()}
           </ul>
         </nav>
       </div>
@@ -16,4 +36,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
