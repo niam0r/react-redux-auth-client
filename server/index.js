1ed55d5067e921a9bcdc8b807ac5d037431dafbf
Romain niam0r <niamor@mm.st>
2018-04-06 12:25:16 +0100

    copied server from https://github.com/niam0r/react-redux-auth-server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// main start point of the app
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB setup
+mongoose.connect('mongodb://localhost:27017/auth');
+//                     this is my db mame: /auth
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on:', port);
+
+// nodemon is a sort of hot restarter for the server folder
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,16 @@
-import thunk from 'redux-thunk';
 
-export function signinuser({ email, password }) {
-  // submit email and password to the server
 
-  // if req is good:
-  // - update state to indicate user is auth'd
-  // - save the JWT token
-  // - redirect to the route '/feature'
+export function signinUser({ email, password }) {
+  // redux-thunk give us access to dispatch
+  return function(dispatch) {
+    //  submit email and password to the server
 
-  // if req is bad:
-  // - show error to the user
+    // if req is good:
+    // - update state to indicate user is auth'd
+    // - save the JWT token
+    // - redirect to the route '/feature'
+
+    // if req is bad:
+    // - show error to the user
+  }
 }
