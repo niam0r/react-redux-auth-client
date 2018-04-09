8f490d257452e03db8d30c2c0aafbc91f81deb75
Romain niam0r <niamor@mm.st>
2018-04-09 18:36:21 +0100

    implemented other validations, need DRY it

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -29,6 +29,19 @@ class Signup extends Component {
 
 function validate(formProps) {
   const errors = {};
+  // DRY the statements below
+
+  if (!formProps.email) {
+    errors.email = "Please enter an email";
+  }
+
+  if (!formProps.password) {
+    errors.password = "Please enter a password";
+  }
+
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = "Please enter a password confirmation";
+  }
 
   if (formProps.password !== formProps.passwordConfirm) {
     errors.password = "Passwords must match";
